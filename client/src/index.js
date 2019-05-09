dbf7222064cdbcfe74779f6b23a71fca271a86c3
sborzova <borzovasilvia@gmail.com>
2019-05-09 18:52:19 +0200

    removed unused  store, reducers

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 import Keycloak from 'keycloak-js';
-import {applyMiddleware, createStore} from "redux";
-import reducer from "./stores/store";
-import {Provider} from "react-redux";
-import thunk from "redux-thunk";
 import axios from "axios";
-//const store = createStore(reducer, applyMiddleware(thunk));
 
 const kc = Keycloak('./keycloak.json');
 const token = localStorage.getItem('kc_token');
@@ -19,11 +14,8 @@ const refreshToken = localStorage.getItem('kc_refreshToken');
 kc.init({onLoad: 'login-required', promiseType: 'native', token, refreshToken})
     .then(authenticated => {
         if (authenticated) {
-            //store.getState().keycloak = kc;
             updateLocalStorage();
-            //ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
             ReactDOM.render(<App/>, document.getElementById('root'));
-
         }
     });
 
